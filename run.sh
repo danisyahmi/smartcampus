bcf3cf486bd3138040d65f0790953b29370450bb
danisyahmi <danisyahmiii@gmail.com>
2026-06-19 03:43:21 +0800

    update run script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "up" ]; then
     echo "Bring containers UP..."
-    docker compose up --build -d
+    docker compose up -d --force-recreate --remove-orphans
     sleep 5
     docker compose ps
 elif [ "$1" == "down" ]; then
