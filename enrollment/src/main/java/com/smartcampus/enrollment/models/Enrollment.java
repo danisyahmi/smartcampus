10d39fe38943f64cd7417750722f904f9d1de5f4
Your Full Name <miminridzuan44@gmail.com>
2026-06-20 19:19:48 +0800

    update enrollment service and controller

--- enrollment/src/main/java/com/smartcampus/enrollment/models/Enrollment.java
+++ enrollment/src/main/java/com/smartcampus/enrollment/models/Enrollment.java
@@ -1,13 +1,38 @@
 package com.smartcampus.enrollment.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "enrollments")
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Enrollment {
-    private int enrollmentId;
-    private int studentId;
-    private String courseCode;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // primary key
+
+    @Column(name = "student_id", nullable = false)
+    private String studentId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course; // foreign key from table Course
+
+    @Column(nullable = false)
     private String semester;
 
-    public Enrollment() {}
+    @Column(nullable = false)
+    private String status;
 }
