e575f8b47a2081cb004ce3081a1087d73dcd4b5e
danisyahmi <danisyahmiii@gmail.com>
2026-06-20 18:36:46 +0800

    update dto file

--- enrollment/src/main/java/com/smartcampus/enrollment/dto/EnrollmentEvent.java
+++ enrollment/src/main/java/com/smartcampus/enrollment/dto/EnrollmentEvent.java
@@ -1,17 +1,17 @@
 package com.smartcampus.enrollment.dto;
 
 import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EnrollmentEvent implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String studentEmail;
     private String courseName;
-
-    public EnrollmentEvent() {}
-
-    public EnrollmentEvent(String studentEmail, String courseName) {
-        this.studentEmail = studentEmail;
-        this.courseName = courseName;
-    }
+    private String semester;
 }
